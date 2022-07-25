5ede7330f3f2f1b894d7317eeea596f26f87a680
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-25 09:44:24 -0600

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :employee_tickets
   resources :tickets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :employees
+  resources :departments
 end
