a15f696796f44aea0911398dc1e3db8974aec5cc
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-25 09:41:43 -0600

    Add db seeds

---
+++ spec/features/departments/index_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+# As a user,
+# When I visit the Department index page,
+# I see each department's name and floor
+# And underneath each department, I can see the names of all of its employees
+
+RSpec.describe 'Department index page' do
+  
