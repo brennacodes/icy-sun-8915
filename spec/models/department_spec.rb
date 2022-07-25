0b90bb34392d0f05668091da6dd47e5978e58cae
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-25 12:56:25 -0600

    Test: add department model spec; pass

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe Department, type: :model do
   describe 'relationships' do
     it { should have_many :employees }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :floor }
+  end
+
+  describe 'instance methods' do
+    it 'lists employees in a department' do
+      department = Department.create(name: 'Engineering', floor: 1)
+      department_2 = Department.create(name: 'Engineering', floor: 2)
+      employee_2 = Employee.create!(name: 'Bob', level: 2, department_id: department.id)
+      employee_1 = Employee.create!(name: 'Alice', level: 1, department_id: department.id)
+      employee_3 = Employee.create!(name: 'Charlie', level: 3, department_id: department_2.id)
+
+      expect(department.list_employees).to eq 'Bob, Alice'
+    end
+  end
 end
