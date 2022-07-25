93b1a7c68349c45f158e4db85b694f536ebc3814
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-25 10:23:11 -0600

    Test: dept_index page shows department's employees

--- spec/features/departments/index_spec.rb
+++ spec/features/departments/index_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe 'Department index page' do
   let!(:department_1) {Department.create(name: 'Engineering', floor: 1)}
   let!(:department_2) {Department.create(name: 'HR', floor: 2)}
   let!(:department_3) {Department.create(name: 'Marketing', floor: 3)}
+  let!(:employee_1) {Employee.create(name: 'John', level: 1, department_id: department_1.id)}
+  let!(:employee_2) {Employee.create(name: 'Jane', level: 1, department_id: department_1.id)}
+  let!(:employee_3) {Employee.create(name: 'Joe', level: 2, department_id: department_2.id)}
+  let!(:employee_4) {Employee.create(name: 'Jill', level: 3, department_id: department_2.id)}
+  let!(:employee_5) {Employee.create(name: 'Jack', level: 3, department_id: department_3.id)}
 
   it "renders a list of departments" do
     visit departments_path
@@ -23,12 +28,17 @@ RSpec.describe 'Department index page' do
     within 'table' do
       expect(page).to have_content(department_1.name)
       expect(page).to have_content(department_1.floor)
+      expect(page).to have_content(employee_1.name)
+      expect(page).to have_content(employee_2.name)
       expect(page).to have_content(department_2.name)
       expect(page).to have_content(department_2.floor)
+      expect(page).to have_content(employee_3.name)
+      expect(page).to have_content(employee_4.name)
       expect(page).to have_content(department_3.name)
       expect(page).to have_content(department_3.floor)
+      expect(page).to have_content(employee_5.name)
     end
   end
 
-  
+
 end
